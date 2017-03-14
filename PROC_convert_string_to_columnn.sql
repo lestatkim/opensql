cc7eb6a4c788b22174bd2fe0cba6dda16b4c5492
lestat <lestatkim@yandex.ru>
2017-03-14 21:24:38 +0300

    edit comment's

--- PROC_convert_string_to_columnn.sql
+++ PROC_convert_string_to_columnn.sql
@@ -1,39 +1,34 @@
-/*	Parse string to table
-	arguments : (string, delimeter)
-	example : select * from (N'one/two/three/four/five', '/')
-*/
+--
+-- Parse string to table
+-- arguments : (string, delimeter)
+-- example : select * from (N'one/two/three/four/five', '/')
 
-create function string_to_table(
+
+CREATE FUNCTION string_to_table(
 	 @str nvarchar(max)
 	,@delimeter nchar(1)
 )
-returns @t table (name nvarchar(max))
+returns @t table (
+	name nvarchar(max)
+)
 as BEGIN
 		
-	declare @i int;
+	declare @i int
 
-	set @i = CHARINDEX(@delimeter, @str);
+	set @i = CHARINDEX(@delimeter, @str)
 
 	while 1 = 1 begin
 		if @i != 0 begin
-			insert @t values (left(@str, @i - 1));
-				set @str = right(@str, len(@str) - @i);
-				set @i = CHARINDEX(@delimeter, @str);
+			insert @t values (left(@str, @i - 1))
+				set @str = right(@str, len(@str) - @i)
+				set @i = CHARINDEX(@delimeter, @str)
 				continue;
-		end;
+		end
 		else
 			insert @t values(@str)
 			set @str = null
 			break;
 	end;
 
-
 return
 END
-
-set @i = null;
-set @t = null;
-go
-
-
-
