c0589d7550f2f63f18f303734024d7b686797026
lestat <lestatkim@yandex.ru>
2017-03-14 22:12:19 +0300

    +1

--- PROC_convert_string_to_columnn.sql
+++ PROC_convert_string_to_columnn.sql
@@ -1,53 +1,40 @@
---
-<<<<<<< HEAD
--- Convert string to column
--- arguments : (string, delimeter)
--- example : select * from (N'one/two/three/four/five', '/')
+--Convert string to column
+use tempdb
+go
 
+/*  @example :
+    use tempdb
+    go
 
-CREATE FUNCTION string_to_column(
-	 @str nvarchar(max)
-	,@delimeter nchar(1)
-)
-returns @t table (
-	name nvarchar(max)
-)
-=======
-/*	Parse string to table
-	arguments : (string, delimeter)
-	example : select * from (N'one/two/three/four/five', '/')
+    select * from ( N'one/two/three/four/five', '/' ) 
 */
 
-CREATE FUNCTION string_to_table(
-	 @str nvarchar(max)
-	,@delimeter nchar(1)
-)
-returns @t table ( name nvarchar(max) )
->>>>>>> a90bc14a79430bb5d3904bc26306c0579432910b
-as BEGIN
-		
-	declare @i int
+create function String_to_column(  @str nvarchar(max), @delimeter nchar(1) )
+    
+    /*  don't forget to click on Star ;) if u like it
+    */
+    returns @t table ( name nvarchar(max) )
+    as BEGIN
+	
+	    declare @i int
+    	set @i = CHARINDEX(@delimeter, @str)
 
-	set @i = CHARINDEX(@delimeter, @str)
+	    while 1 = 1 begin
+    		if @i != 0 begin
+    			insert @t values (left(@str, @i - 1))
+	    		set @str = right(@str, len(@str) - @i)
+		    	set @i = CHARINDEX(@delimeter, @str)
+			   	continue;
+    		end;
 
-	while 1 = 1 begin
-		if @i != 0 begin
-			insert @t values (left(@str, @i - 1))
-				set @str = right(@str, len(@str) - @i)
-				set @i = CHARINDEX(@delimeter, @str)
-				continue;
-		end
-		else
-			insert @t values(@str)
-			set @str = null
-			break;
-	end;
+	    	else
+		    	insert @t values(@str)
+			    set @str = null
+    			break;
+    	end;
 
-return
-END
-<<<<<<< HEAD
-=======
+    return
+    END
 
+--https://github.com/lestatkim/opensql
 
-
->>>>>>> a90bc14a79430bb5d3904bc26306c0579432910b
