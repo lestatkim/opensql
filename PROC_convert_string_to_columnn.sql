53bab0829e86a85a59b9f24aaa69f25d91f73cfe
lestat <lestatkim@yandex.ru>
2017-03-14 22:23:50 +0300

    +1

--- PROC_convert_string_to_columnn.sql
+++ PROC_convert_string_to_columnn.sql
@@ -19,22 +19,25 @@ create function String_to_column(  @str nvarchar(max), @delimeter nchar(1) )
         declare @i int
         set @i = CHARINDEX(@delimeter, @str)
 
-	    while 1 = 1 begin
-    		if @i != 0 begin
-    			insert @t values (left(@str, @i - 1))
-	    		set @str = right(@str, len(@str) - @i)
-		    	set @i = CHARINDEX(@delimeter, @str)
-			   	continue;
-    		end;
+	    while 1 = 1 
+            begin
+    		    if @i != 0
+                    begin
+    			        insert @t values ( left(@str, @i - 1) );
 
-	    	else
-		    	insert @t values(@str)
-			    set @str = null
-    			break;
-    	end;
+	    		        set @str = right( @str, len(@str) - @i )
+		    	        set @i = CHARINDEX( @delimeter, @str )
+			   	        continue;
+    		        end;
+
+	    	    else
+		    	    insert @t values(@str)
+			        set @str = null
+    			    break;
+    	    end;
 
     return
     END
 
---github@lestatkim/opensql
+--github@LestatKim/openSql
 
