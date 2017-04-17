8f18c9bdd8f26ef41fe9376bb687d80df87f95b9
lestat <lestatkim@yandex.ru>
2017-04-17 12:29:16 +0300

    add recursive method

--- factorial.sql
+++ factorial.sql
@@ -28,3 +28,40 @@ as begin
 	return @result
 end;
 go
+
+-----------------------------------------------------------
+
+
+if object_id('dbo.factorial', 'FN') is not null
+	drop function dbo.factorial;
+go
+create function dbo.factorial(@n int)
+/*	Описание: Функция вычисляет факториал Рекурсивно
+	от заданного числа. 
+	Справка: факториал числа 5 это 1 * 2 * 3 * 4 * 5
+	Пример: select dbo.factorial(7)
+*/
+returns int
+as begin
+	declare @result int;
+
+	with
+		_f(n, r) as (
+			select 1, 1
+		union all
+			select
+				n + 1
+				,r * (n + 1)
+			from _f 
+			where n !> 6
+		)
+		,f as (
+			select r 
+			from _f
+			where n = @n
+		)
+	select @result = r from f;
+
+	return @result
+end;
+go
