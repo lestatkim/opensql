96f0db44185923271c909c359b775839fa4de51c
lestat <lestatkim@yandex.ru>
2017-06-15 17:30:32 +0300

    New file

---
+++ backup_age.sql
@@ -0,0 +1,22 @@
+/*  Author: Sergey Lobanov
+/*
+
+WITH LastRestores AS
+	(
+		SELECT
+		    DatabaseName = [d].[name]
+		    , [d].[create_date]
+		    , [d].[compatibility_level]
+		    , [d].[collation_name]
+		    , r.*
+		    , RowNum = ROW_NUMBER() OVER (
+		    	PARTITION BY d.Name ORDER BY r.[restore_date] DESC
+		    )
+		FROM master.sys.databases AS d
+			LEFT OUTER JOIN msdb.dbo.[restorehistory] AS r
+				ON r.[destination_database_name] = d.Name
+	)
+SELECT *
+FROM [LastRestores]
+WHERE [RowNum] = 1
+
