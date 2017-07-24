36e8ff7475868bd1c46b2efb7386772fff0f9975
lestat <lestatkim@yandex.ru>
2017-07-24 12:16:41 +0300

    add kill_sessions

---
+++ kill_sessions.sql
@@ -0,0 +1,48 @@
+/*  Kill session's
+    Autor: Igor Opium
+*/
+DECLARE @TABLE TABLE
+(
+    id INT
+    , spid INT
+);
+
+DECLARE
+    @id INT
+    , @count INT
+    , @sql NVARCHAR(MAX)
+;
+
+INSERT INTO @table
+    SELECT ROW_NUMBER() OVER (ORDER BY spid)
+        , spid
+    FROM sys.sysprocesses
+    WHERE spid > 50 
+        AND [status] = N'sleeping'
+        AND open_tran = 0 
+        AND spid != @@spid
+        AND DATEDIFF( mi, last_batch, GETDATE() ) !< 180
+;
+
+SELECT @count = count(spid)
+FROM @table
+;
+
+SET @id = 1
+;
+
+WHILE @id !> @count
+BEGIN
+    SET @sql = 'kill ' 
+        + (
+            SELECT CAST( spid AS NVARCHAR(20) )
+            FROM @table 
+            WHERE id = @id
+        )
+    ;
+    EXEC (@sql)
+    ;
+    SET @id += 1
+    ;
+END
+;
