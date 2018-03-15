3b3b55309e7a335dd1418615ab2feb7520c9d515
lestat <lestatkim@yandex.ru>
2018-03-15 11:27:39 +0300

    +1

--- columnStoreIndexReorganizesql.sql
+++ columnStoreIndexReorganizesql.sql
@@ -1,62 +1,75 @@
-DECLARE @schemaName VARCHAR(250)
-DECLARE @tableName VARCHAR(250) 
-DECLARE @indexName VARCHAR(250)
-DECLARE @defrag FLOAT
-DECLARE @partition_number VARCHAR(250)
-DECLARE @max_partition_number VARCHAR(250)
-DECLARE @sql NVARCHAR(MAX)
-DECLARE @database_name nvarchar(255)
-DECLARE @columnstore_exists int
-DECLARE @indexes_count int
-DECLARE @columnstore_indexes_count int
+DECLARE @schemaName VARCHAR(250);
+DECLARE @tableName VARCHAR(250);
+DECLARE @indexName VARCHAR(250);
+DECLARE @defrag FLOAT;
+DECLARE @partition_number VARCHAR(250);
+DECLARE @max_partition_number VARCHAR(250);
+DECLARE @sql NVARCHAR(MAX);
+DECLARE @database_name NVARCHAR(255);
+DECLARE @columnstore_exists INT;
+DECLARE @indexes_count INT;
+DECLARE @columnstore_indexes_count INT;
 
-SET @database_name = DB_ID()
-SET @indexes_count = 0
-SET @columnstore_indexes_count = 0
+SET @database_name = DB_ID();
+SET @indexes_count = 0;
+SET @columnstore_indexes_count = 0;
 
--- Приступаем к обновлению колоночных индексов если версия SQL Server 2014+
-IF CAST(SUBSTRING(CAST(SERVERPROPERTY ('productversion') as nvarchar(50)),1,2) as int) > 11
+-- РџСЂРёСЃС‚СѓРїР°РµРј Рє РѕР±РЅРѕРІР»РµРЅРёСЋ РєРѕР»РѕРЅРѕС‡РЅС‹С… РёРЅРґРµРєСЃРѕРІ РµСЃР»Рё РІРµСЂСЃРёСЏ SQL Server 2014+
+IF CAST(SUBSTRING(CAST(SERVERPROPERTY('productversion') AS NVARCHAR(50)), 1, 2) AS INT) > 11
 BEGIN
 
-DECLARE @type int
+    DECLARE @type INT;
 
-SET @schemaName = ''
-SET @tableName = ''
-SET @indexName = ''
-SET @partition_number = ''
-SET @max_partition_number = ''
-SET @defrag = ''
-SET @sql = ''
+    SET @schemaName = '';
+    SET @tableName = '';
+    SET @indexName = '';
+    SET @partition_number = '';
+    SET @max_partition_number = '';
+    SET @defrag = '';
+    SET @sql = '';
 
 
-DECLARE defragCC CURSOR FOR
+    DECLARE defragCC CURSOR FOR
+    SELECT '[' + sh.name + ']',
+           '[' + OBJECT_NAME(i.object_id) + ']' AS TableName,
+           '[' + i.name + ']' AS IndexName,
+           MAX(100 * (ISNULL(deleted_rows, 0)) / total_rows) AS 'Fragmentation',
+           partition_number,
+           MAX(partition_number) OVER (PARTITION BY i.name),
+           MAX(i.[type])
+    FROM sys.indexes AS i
+        INNER JOIN sys.tables AS tbl
+            ON i.object_id = tbl.object_id
+        INNER JOIN sys.schemas AS sh
+            ON sh.schema_id = tbl.schema_id
+        INNER JOIN sys.dm_db_column_store_row_group_physical_stats AS CSRowGroups
+            ON i.object_id = CSRowGroups.object_id
+               AND i.index_id = CSRowGroups.index_id
+    WHERE CSRowGroups.deleted_rows > 0
+          AND CSRowGroups.total_rows > 0
+    GROUP BY sh.name,
+             OBJECT_NAME(i.object_id),
+             i.name,
+             partition_number
+    HAVING MAX(100 * (ISNULL(deleted_rows, 0)) / total_rows) > 20
+    ORDER BY MAX(100 * (ISNULL(deleted_rows, 0)) / total_rows) DESC;
 
-	SELECT '['+sh.name+']',   
-		'['+object_name(i.object_id)+']' AS TableName,   
-		'['+i.name+']' AS IndexName,   
-	    MAX(100*(ISNULL(deleted_rows,0))/total_rows) AS 'Fragmentation' , 
-		partition_number,
-		MAX (partition_number) OVER (PARTITION BY i.name),
-		MAX(i.[type])
-	FROM sys.indexes AS i  
-		INNER JOIN sys.tables tbl ON i.object_id = tbl.object_id
-		INNER JOIN sys.schemas sh ON sh.schema_id = tbl.schema_id
-		INNER JOIN sys.dm_db_column_store_row_group_physical_stats AS CSRowGroups  
-		ON i.object_id = CSRowGroups.object_id AND i.index_id = CSRowGroups.index_id
-		WHERE CSRowGroups.deleted_rows > 0 AND CSRowGroups.total_rows > 0
-		GROUP BY sh.name,object_name(i.object_id),i.name,partition_number
-		HAVING MAX(100*(ISNULL(deleted_rows,0))/total_rows) > 20
-		ORDER BY MAX(100*(ISNULL(deleted_rows,0))/total_rows) DESC
+    OPEN defragCC;
+    FETCH NEXT FROM defragCC
+    INTO @schemaName,
+         @tableName,
+         @indexName,
+         @defrag,
+         @partition_number,
+         @max_partition_number,
+         @type;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
 
-OPEN defragCC
-FETCH NEXT FROM defragCC INTO @schemaName,@tableName, @indexName, @defrag,@partition_number,@max_partition_number,@type
-WHILE @@FETCH_STATUS=0
-BEGIN
-
-	IF OBJECT_ID(@schemaName+'.'+@tableName,'U') is not null
-	BEGIN
-		BEGIN TRY				
-				/* -- Отключил так как даже на 2016 появляются проблемы с ONLINE = ON
+        IF OBJECT_ID(@schemaName + '.' + @tableName, 'U') IS NOT NULL
+        BEGIN
+            BEGIN TRY
+                /* -- РћС‚РєР»СЋС‡РёР» С‚Р°Рє РєР°Рє РґР°Р¶Рµ РЅР° 2016 РїРѕСЏРІР»СЏСЋС‚СЃСЏ РїСЂРѕР±Р»РµРјС‹ СЃ ONLINE = ON
 				IF @type = 6 and (SELECT @@VERSION) LIKE '%Enterprise Edition%'
 					IF @max_partition_number > 1
 						SET @sql = 'ALTER INDEX '+@indexName+' ON '+@schemaName+'.'+@tableName+' Rebuild PARTITION = '+@partition_number+' WITH (ONLINE = ON);'
@@ -64,43 +77,62 @@ BEGIN
 						SET @sql = 'ALTER INDEX '+@indexName+' ON '+@schemaName+'.'+@tableName+' Rebuild PARTITION = ALL WITH (ONLINE = ON);'
 				ELSE
 				BEGIN */
-					IF @max_partition_number > 1
-						BEGIN
-							SET @sql = 'ALTER INDEX '+@indexName+' ON '+@schemaName+'.'+@tableName+' REORGANIZE PARTITION = '+@partition_number+' WITH (COMPRESS_ALL_ROW_GROUPS = ON);
+                IF @max_partition_number > 1
+                BEGIN
+                    SET @sql
+                        = 'ALTER INDEX ' + @indexName + ' ON ' + @schemaName + '.' + @tableName
+                          + ' REORGANIZE PARTITION = ' + @partition_number
+                          + ' WITH (COMPRESS_ALL_ROW_GROUPS = ON);
 							
-							ALTER INDEX '+@indexName+' ON '+@schemaName+'.'+@tableName+' REORGANIZE PARTITION = '+@partition_number
+							ALTER INDEX ' + @indexName + ' ON ' + @schemaName + '.' + @tableName
+                          + ' REORGANIZE PARTITION = ' + @partition_number;
 
-						END
-					ELSE 
-						BEGIN
+                END;
+                ELSE
+                BEGIN
 
-							SET @sql = 'ALTER INDEX '+@indexName+' ON '+@schemaName+'.'+@tableName+' REORGANIZE PARTITION = ALL WITH (COMPRESS_ALL_ROW_GROUPS = ON);
+                    SET @sql
+                        = 'ALTER INDEX ' + @indexName + ' ON ' + @schemaName + '.' + @tableName
+                          + ' REORGANIZE PARTITION = ALL WITH (COMPRESS_ALL_ROW_GROUPS = ON);
 							
-							ALTER INDEX '+@indexName+' ON '+@schemaName+'.'+@tableName+' REORGANIZE PARTITION = ALL'
+							ALTER INDEX ' + @indexName + ' ON ' + @schemaName + '.' + @tableName
+                          + ' REORGANIZE PARTITION = ALL';
 
-						END
-				--END
+                END;
+                --END
 
-				--print @sql
-				exec (@sql)
-				
-				SET @columnstore_indexes_count = @columnstore_indexes_count +1 
+                --print @sql
+                EXEC (@sql);
 
-		END TRY
-		BEGIN CATCH
+                SET @columnstore_indexes_count = @columnstore_indexes_count + 1;
 
-				SET @database_name = DB_NAME()
-				INSERT INTO master.dbo.dbmaintenance 
-				VALUES (@database_name,'INDEX - Номер ошибки '+CAST(ERROR_NUMBER() as nvarchar(50)) + ', в строке ' + CAST(ERROR_LINE() as nvarchar(50)) +'. Сообщение об ошибке: ' + CAST(ERROR_MESSAGE() as nvarchar(400)),GETDATE())
+            END TRY
+            BEGIN CATCH
 
-		END CATCH
-	END
-    FETCH NEXT FROM defragCC INTO @schemaName,@tableName, @indexName, @defrag,@partition_number,@max_partition_number,@type
-END
-CLOSE defragCC
-DEALLOCATE defragCC
+                SET @database_name = DB_NAME();
+                INSERT INTO master.dbo.dbmaintenance
+                VALUES
+                (@database_name,
+                 'INDEX - РќРѕРјРµСЂ РѕС€РёР±РєРё ' + CAST(ERROR_NUMBER() AS NVARCHAR(50)) + ', РІ СЃС‚СЂРѕРєРµ '
+                 + CAST(ERROR_LINE() AS NVARCHAR(50)) + '. РЎРѕРѕР±С‰РµРЅРёРµ РѕР± РѕС€РёР±РєРµ: '
+                 + CAST(ERROR_MESSAGE() AS NVARCHAR(400)), GETDATE());
 
-END
+            END CATCH;
+        END;
+        FETCH NEXT FROM defragCC
+        INTO @schemaName,
+             @tableName,
+             @indexName,
+             @defrag,
+             @partition_number,
+             @max_partition_number,
+             @type;
+    END;
+    CLOSE defragCC;
+    DEALLOCATE defragCC;
 
-print 'Columnstore Indexes done - '+ CAST(@columnstore_indexes_count as nvarchar(50))
+END;
+
+PRINT 'Columnstore Indexes done - ' + CAST(@columnstore_indexes_count AS NVARCHAR(50));
+
 
