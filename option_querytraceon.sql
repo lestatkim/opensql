7559f407dfda41f8f49416ce6d31bca2a87389ab
lestat <lestatkim@yandex.ru>
2018-07-19 10:28:13 +0300

    +1

---
+++ option_querytraceon.sql
@@ -0,0 +1,13 @@
+
+OPTION  ( 
+	  querytraceon 3604 -- Console output
+	, querytraceon 8606 -- LogOp Tree
+	, querytraceon 8607 -- PhysOp Tree 
+	, querytraceon 8619 -- Applied Transformation Rules
+	, querytraceon 8612 -- Add Extra Info to the Trees Output
+	, querytraceon 2372 -- Memory for Phases
+	, querytraceon 2373 -- Memory for Deriving Properties
+	, querytraceon 2363 -- (2014, new:) TF Selectivity
+	, querytraceon 8608 -- start memo
+	, querytraceon 8615 -- end memo
+);
