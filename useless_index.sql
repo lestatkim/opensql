edc3af27e1d6960d7a741621f2e2f1e550e1cf4d
lestat <lestatkim@yandex.ru>
2018-08-06 10:20:22 +0300

    +1

---
+++ useless_index.sql
@@ -0,0 +1,28 @@
+
+/*	Useless index
+*/
+DECLARE @table_name sysname = 'AccountingDocumentSegment';
+
+SELECT OBJECT_NAME(A.[OBJECT_ID]) AS [OBJECT NAME], 
+       I.[NAME] AS [INDEX NAME], 
+       A.LEAF_INSERT_COUNT, 
+       A.LEAF_UPDATE_COUNT, 
+       A.LEAF_DELETE_COUNT 
+FROM   SYS.DM_DB_INDEX_OPERATIONAL_STATS (db_id(),NULL,NULL,NULL ) A 
+       INNER JOIN SYS.INDEXES AS I 
+         ON I.[OBJECT_ID] = A.[OBJECT_ID] 
+            AND I.INDEX_ID = A.INDEX_ID 
+WHERE  OBJECTPROPERTY(A.[OBJECT_ID], @table_name) = 1
+;
+
+SELECT OBJECT_NAME(S.[OBJECT_ID]) AS [OBJECT NAME], 
+       I.[NAME] AS [INDEX NAME], 
+       USER_SEEKS, 
+       USER_SCANS, 
+       USER_LOOKUPS, 
+       USER_UPDATES 
+FROM   SYS.DM_DB_INDEX_USAGE_STATS AS S 
+       INNER JOIN SYS.INDEXES AS I ON I.[OBJECT_ID] = S.[OBJECT_ID] AND I.INDEX_ID = S.INDEX_ID 
+WHERE  OBJECTPROPERTY(S.[OBJECT_ID],'IsUserTable') = 1
+       AND S.database_id = DB_ID()
+	   AND OBJECT_NAME(S.[OBJECT_ID]) = @table_name;
